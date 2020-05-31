e3f3bd5e056914fd72359fe4ef481a717970bc04
Dhiraj <sharmadhiraj2040@gmail.com>
2020-05-31 20:17:36 +0530

    Initial commit

---
+++ src/test/java/com/dhirajsharma/messaging/standaloneactivemqexample/StandaloneActivemqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhirajsharma.messaging.standaloneactivemqexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandaloneActivemqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
